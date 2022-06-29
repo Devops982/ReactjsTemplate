d7989b1d50b587f17a305e9506270322774ff541
“Enter <talktoparthi@gmail.com>
2022-06-29 10:53:01 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBarcomp from "./components/NavBarcomp";
+import React from "react";
+import Cardsw from "./components/Cardsw";
+import login from "./components/login";
+import { FormLabel } from "react-bootstrap";
+import carousel1 from "./components/carousel1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBarcomp />
+      <carousel1 />
+      <Cardsw />
+      <FormLabel />
+      <login />
     </div>
   );
 }
