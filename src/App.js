03868f760030cf7bf9ccce9f42acf629dc03b7e2
Parthiban <65107540+Devops982@users.noreply.github.com>
2022-06-30 13:55:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 function App() {
-  return <div>Hello world</div>;
+  return <div className="container">
+      <h1>Hello, World</h1>
+      <p>I am writing JSX</p>
+    </div>;
 }
 
 export default App;
